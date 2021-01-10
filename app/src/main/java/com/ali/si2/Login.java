94263c9ac0a65cb8323bd055f4e886f023641d79
AliCuchalloGabrielFerando <f1.gabriel73@gmail.com>
2021-01-09 21:18:45 -0400

    Login

---
+++ app/src/main/java/com/ali/si2/Login.java
@@ -0,0 +1,18 @@
+package com.ali.si2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void iniciar(View view) {
+    }
+}
